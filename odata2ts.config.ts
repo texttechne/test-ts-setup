7a4dc24524599e10b2a89b3ab5dd0e16f8515b6d
texttechne <gedankengut@gmx.de>
2023-09-24 15:48:34 +0200

    feat: adding odata2ts for client generation

---
+++ odata2ts.config.ts
@@ -0,0 +1,17 @@
+import {ConfigFileOptions, EmitModes, Modes} from "@odata2ts/odata2ts";
+
+
+const config: ConfigFileOptions = {
+  mode: Modes.all,
+  emitMode: EmitModes.ts,
+  services: {
+    rhymba: {
+      sourceUrl: 'https://search.mcnemanager.com/dev/content.odata/',
+      source: 'src/resource/rhymba.xml',
+      output: 'src/build/rhymba',
+      prettier: true,
+    },
+  },
+}
+
+export default config;
